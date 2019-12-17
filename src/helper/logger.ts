3576c64b554270f2511a3ef5eca6c7b37ced73ee
xaviraol <xaviramos3@gmail.com>
2019-12-17 15:40:47 +0100

    Commented out reactotron

--- src/helper/logger.ts
+++ src/helper/logger.ts
@@ -15,26 +15,37 @@ let logger: {
     native: (...anything: any[]) => void;
 };
 
-if (__DEV__) {
+// if (__DEV__) {
     // tslint:disable-next-line:no-var-requires
-    const Reactotron = require('reactotron-react-native').default;
-    logger = {
-        error: Reactotron.error,
-        warn: Reactotron.warn,
-        debug: Reactotron.log,
-        native: nativeLogger,
-    };
-} else {
-    logger = {
-        // tslint:disable-next-line:no-console
-        error: console.error,
-        // tslint:disable-next-line:no-console
-        warn: console.warn,
-        // tslint:disable-next-line:no-console
-        debug: console.log,
-        // tslint:disable-next-line:no-console
-        native: nativeLogger,
-    };
-}
+    // TODO: we shouldn't be using reactotron in react-native-inputkit.
+    // const Reactotron = require('reactotron-react-native').default;
+    // logger = {
+    //     error: Reactotron.error,
+    //     warn: Reactotron.warn,
+    //     debug: Reactotron.log,
+    //     native: nativeLogger,
+    // };
+// } else {
+    // logger = {
+    //     // tslint:disable-next-line:no-console
+    //     error: console.error,
+    //     // tslint:disable-next-line:no-console
+    //     warn: console.warn,
+    //     // tslint:disable-next-line:no-console
+    //     debug: console.log,
+    //     // tslint:disable-next-line:no-console
+    //     native: nativeLogger,
+    // };
+// }
+logger = {
+    // tslint:disable-next-line:no-console
+    error: console.error,
+    // tslint:disable-next-line:no-console
+    warn: console.warn,
+    // tslint:disable-next-line:no-console
+    debug: console.log,
+    // tslint:disable-next-line:no-console
+    native: nativeLogger,
+};
 
 export default logger;
