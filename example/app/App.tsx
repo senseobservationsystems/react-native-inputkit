d5d2b52194db4ca9db15f774456f2a6c603797ce
xaviraol <xaviramos3@gmail.com>
2019-12-18 16:15:54 +0100

    First couple of examples of usage

---
+++ example/app/App.tsx
@@ -0,0 +1,93 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {Fragment, useState, useEffect, useCallback} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TouchableHighlight
+} from 'react-native';
+
+import * as HealthManager from './manager';
+
+const App = () => {
+  /**
+   * isAvailable
+   * 
+   * To check wether Healthkit (iOS) or GoogleFit (Android) are available on the device
+   */
+  const [isHealthAvailable, setIsHealthAvailable] = useState();
+  useEffect(() => setIsHealthAvailable(HealthManager.isAvailable()), []);
+  
+  /**
+   * requestPermissions
+   * 
+   * To request user's permissions to use Healthkit (iOS) data.
+   */
+  const [arePermissionsGranted, setArePermissionsGranted] = useState();
+  const requestPermissions = useCallback(async () => {
+    try {
+      await HealthManager.requestPermissions(['stepCount']);
+      setArePermissionsGranted(true);
+    } catch (e){
+      console.log(`==== Rejected requestRermissions: ${JSON.stringify(e)}`)
+      setArePermissionsGranted(false);
+    }
+  },[]);
+
+  /**
+   * getStepCount
+   * 
+   * Returns all the steps between the two argument dates
+   */
+
+   const [stepCount, setStepCount] = useState();
+   const getStepCount = useCallback(async () => {
+    try {
+      const steps = await HealthManager.getStepCount(new Date('2019-12-01'), new Date('2019-12-18'));
+      setStepCount(steps);
+    } catch (e){
+      console.log(`==== Rejected getStepCount: ${JSON.stringify(e)}`)
+    }
+  }, []);
+
+  return (
+    <Fragment>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <View style={{height: 20, width: 20, backgroundColor: isHealthAvailable ? 'green' : 'red'}} />
+          <View style={styles.body}>
+            <TouchableHighlight style={{ height: 100, alignItems: 'center', justifyContent: 'center', backgroundColor: '#BBBBBB' }} onPress={requestPermissions}><Text>{arePermissionsGranted ? 'Permissions granted' : 'Request Permissions'}</Text></TouchableHighlight>
+            <View style={{height: 20, width: 20}} />
+            <TouchableHighlight style={{ height: 100, alignItems: 'center', justifyContent: 'center', backgroundColor: '#BBBBBB' }} onPress={getStepCount}><Text>{`Get step Count: ${stepCount}`}</Text></TouchableHighlight>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: 'white',
+  },
+  body: {
+    backgroundColor: 'white',
+  },
+});
+
+export default App;
---
+++ example/app/manager.ts
@@ -0,0 +1,14 @@
+import { Health, SampleType } from 'react-native-inputkit';
+
+
+export async function isAvailable(){
+    return Health.reqSharedInstance().then(h => h.isAvailable());
+}
+
+export async function requestPermissions(sampleTypes: SampleType[]){
+    return Health.reqSharedInstance().then(h => h.requestPermissions(sampleTypes))
+}
+
+export async function getStepCount(start: Date, end: Date) {
+    return Health.reqSharedInstance().then(h =>h.getStepCount(start, end))
+}
